797dffd234ab1dca3f1ddc7db45e2e3e1731d314
Mahmood <mahmood.30004@gmail.com>
2019-10-23 13:27:16 +0100

    Auto stash before merge of "master" and "origin/master"

--- Withering/Assets/FollowPlayer.cs
+++ Withering/Assets/FollowPlayer.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,3 +15,21 @@ public class FollowPlayer : MonoBehaviour
         transform.position = player.position + offset;
     }
 }
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform player;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offset;
+    }
+}
+>>>>>>> Stashed changes
--- Withering/Assets/PlayerMovement.cs
+++ Withering/Assets/PlayerMovement.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -36,3 +37,43 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 }
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 500f;
+    public float sidewaysForce = 500f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime , 0 , 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime , 0 , 0);
+        }
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0 , 0 , forwardForce * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0 , 0 , -forwardForce * Time.deltaTime);
+        }
+    }
+}
+>>>>>>> Stashed changes
