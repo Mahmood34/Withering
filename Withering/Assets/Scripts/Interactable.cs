92cded2d2a9d40b1c7445b6f87af4ce81fd39f5f
Mahmood <mahmood.30004@gmail.com>
2019-11-17 23:06:15 +0000

    Player Interaction with objects
    First implementation of player interaction with objects
    Future expansion to this feature includes:
    > Item pickup
    > Scene change
    > NPC interaction etc.

---
+++ Withering/Assets/Scripts/Interactable.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3f;
+    bool isFocus = false;
+    Transform player;
+    bool hasInteracted = false;
+
+    public virtual void Interact()
+    {
+        Debug.Log("Interacting with " + transform.name);
+        
+    }
+
+    void Update()
+    {
+        if (isFocus && !hasInteracted)
+        {
+            Interact();
+            hasInteracted = true;
+        }
+    }
+
+    public void OnFocused()
+    {
+        isFocus = true;
+        hasInteracted = false;
+    }
+
+    public void onDeFocused()
+    {
+        isFocus = false;
+        hasInteracted = false;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+}
---
+++ Withering/Assets/Scripts/MonoBehaviour/Player/PlayerMovement.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Animator animator;
+    public NavMeshAgent agent;
+    public float velocity = 5;
+    public float turnSpeed = 10;
+    public Interactable focus;
+    Vector2 input;
+    float angle;
+    Quaternion targetRotation;
+    public SceneChange sceneChanger;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // agent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        Debug.DrawLine(ray.origin, ray.origin + ray.direction * 5, Color.green);
+
+
+        if (Physics.Raycast(ray, out hit, 5) && Input.GetKeyDown(KeyCode.F))
+        {
+            Interactable interactable = hit.collider.GetComponent<Interactable>();
+            if (interactable != null)
+            {
+                SetFocus(interactable);
+                sceneChanger.TestScene();
+            }
+        }
+        else
+        {
+            RemoveFocus();
+        }
+
+        GetInput();
+        if (Mathf.Abs(input.x) < 1 && Mathf.Abs(input.y) < 1) return;
+        CalculateDirection();
+        Rotate();
+        Move();
+
+    }
+
+    public void SetFocus(Interactable newFocus)
+    {
+        if (newFocus != focus)
+        {
+            if (focus != null)
+                focus.onDeFocused();
+
+            focus = newFocus;
+        }
+
+        newFocus.OnFocused();
+
+    }
+
+    public void RemoveFocus()
+    {
+        if (focus != null)
+            focus.onDeFocused();
+
+        focus = null;
+
+    }
+
+    void GetInput()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void CalculateDirection()
+    {
+        angle = Mathf.Atan2(input.x, input.y);
+        angle = Mathf.Rad2Deg * angle;
+    }
+
+    void Rotate()
+    {
+        targetRotation = Quaternion.Euler(0, angle, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+    }
+
+    void Move()
+    {
+        transform.position += transform.forward * velocity * Time.deltaTime;
+    }
+}
